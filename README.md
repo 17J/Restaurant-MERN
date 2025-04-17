7069fa8c3c07210c272b493f00eb54cd4d145988
17J <17rahuljoshi@gmail.com>
2025-04-17 11:30:29 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Clone this Repo
+
+# stepup .env files
+
+# to Run client move into client folder and run npm install && npm run dev
+
+# to Run Server move into server folder and run npm install && npm start
--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
-
 const mongoose = require('mongoose');
-
+const dbName = process.env.DB_NAME || 'foodMore';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: dbName, // Use the 'dbName' constant
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
